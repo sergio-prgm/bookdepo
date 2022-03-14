82f100a5a62b61b2e20c628de5c168f93115e6fe
Sergio Dev <sergio-dev@outlook.com>
2022-03-14 18:38:27 +0100

    Setup loader interceptor

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LoaderService } from '@app/shared/services/loader.service';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss']
 })
-export class LoaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class LoaderComponent{
+  isLoading$ = this.loaderSvc.isLoading$
+  constructor(private loaderSvc: LoaderService) { }
 
 }
---
+++ src/app/shared/interceptor/loader.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { finalize, Observable } from 'rxjs';
+import { LoaderService } from '../services/loader.service';
+
+@Injectable()
+export class SpinnerInterceptor implements HttpInterceptor {
+  constructor(private loaderSvc: LoaderService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    this.loaderSvc.show();
+    return next.handle(req).pipe(finalize(() => this.loaderSvc.hide()));
+  }
+}
