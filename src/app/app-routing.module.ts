226e59714c41fe10283569316864cf7e765f7bf7
Sergio Dev <sergio-dev@outlook.com>
2022-03-14 11:07:19 +0100

    Setup aliases & routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full'},
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
   { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
   { path: 'favorites', loadChildren: () => import('./pages/favorites/favorites.module').then(m => m.FavoritesModule) },
-  { path: 'book-details', loadChildren: () => import('./pages/book-details/book-details.module').then(m => m.BookDetailsModule) },
+  { path: 'book-details/:id', loadChildren: () => import('./pages/book-details/book-details.module').then(m => m.BookDetailsModule) },
   { path: 'collections', loadChildren: () => import('./pages/collections/collections.module').then(m => m.CollectionsModule) },
-  { path: 'notFound', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }
+  { path: '**', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }
 ];
 
 @NgModule({
