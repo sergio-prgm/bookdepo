003594772d2e5dac5f69bc04ccb8f49ec693374d
Sergio Dev <sergio-dev@outlook.com>
2022-03-18 16:58:13 +0100

    Update files for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: true,
+  apiKey: 'AIzaSyCjnzdqSQ1UnzEOPm6xceX8JJo3l4pLPpQ',
+  searchUrl: 'https://www.googleapis.com/books/v1/volumes',
+  listUrl:
+    'https://www.googleapis.com/books/v1/users/101050476007113578714/bookshelves',
+  bolano: '/1002/volumes',
+  javascript: '/1001/volumes',
 }
