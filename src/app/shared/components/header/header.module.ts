0f84cf0000407b3de1cb20a68e83471e2c625f05
Sergio Dev <sergio-dev@outlook.com>
2022-03-18 12:42:54 +0100

    Change Searchbar to header

--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HeaderComponent } from './header.component'
 import { RouterModule } from '@angular/router'
+import { SearchBarModule } from '../search-bar/search-bar.module'
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SearchBarModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
